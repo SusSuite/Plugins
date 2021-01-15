b98dff923a400685859214ee855fad1351dc9081
Gavin Steinhoff <GavinSteinhoff@gmail.com>
2021-01-15 17:42:05 -0600

    Add project files.

---
+++ src/SusSuite.Plugins.Jester/Models/JesterData.cs
@@ -0,0 +1,8 @@
+﻿namespace SusSuite.Plugins.Jester.Models
+{
+    public class JesterData
+    {
+        public int JesterId { get; set; }
+        public bool BeenNotified { get; set; } = false;
+    }
+}
